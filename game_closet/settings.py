fd972c0c107330eb4fe8e3ff97c25efe22f68fec
ewijohnson <ericwj2@illinois.edu>
2019-05-18 17:45:57 -0500

    host update again

--- game_closet/settings.py
+++ game_closet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u10jz#zgau-==edbzn0+9fi!^jetxuwbta27siw@m)0f@2#_)w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ewijohnson.com', '159.203.88.188', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['ewijohnson.com', '159.203.88.188', '127.0.0.1']
 
 
 # Application definition
